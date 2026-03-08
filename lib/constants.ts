e3bfbd7ec4dc4ce1ed495cedd7d676630c8a9317
R_hmt dev <rohmatulloh.3609@gmail.com>
2026-03-08 16:12:36 +0700

    Create constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,21 @@
+// lib/constants.ts
+export const DEV_INFO = {
+  name: "Rahmat",
+  nickname: "rAi-engine",
+  avatar: "https://res.cloudinary.com/dwiozm4vz/image/upload/v1772959730/ootglrvfmykn6xsto7rq.png",
+  links: {
+    wa: "https://whatsapp.com/channel/0029VbBjyjlJ93wa6hwSWa0p",
+    ig: "https://www.instagram.com/rahmt_nhw",
+    tt: "https://www.tiktok.com/@r_hmtofc",
+    gh: "https://github.com/rahmat-369",
+    tele: "https://t.me/rAi_engine"
+  }
+};
+
+export const NEWS_APIS = [
+  { id: 'cnbc', name: 'CNBC', url: 'https://api.nexray.web.id/berita/cnbcindonesia', color: '#005596' },
+  { id: 'cnn', name: 'CNN', url: 'https://api.nexray.web.id/berita/cnn', color: '#CC0000' },
+  { id: 'kompas', name: 'Kompas', url: 'https://api.nexray.web.id/berita/kompas', color: '#FF9F1C' },
+  { id: 'sindo', name: 'Sindo', url: 'https://api.nexray.web.id/berita/sindonews', color: '#2EB872' },
+  { id: 'suara', name: 'Suara', url: 'https://api.nexray.web.id/berita/suara', color: '#6A0572' }
+];
